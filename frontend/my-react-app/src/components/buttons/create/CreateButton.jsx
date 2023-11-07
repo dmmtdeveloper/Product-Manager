615ca22984a0c480c3ebfb5a6f4e2525ce238381
David Martínez Tapia <dmmtapia.ux@gmail.com>
2023-11-07 00:44:45 -0300

    Administrador de Producto (Parte I)
    "react/prop-types": "off"

--- frontend/my-react-app/src/components/buttons/create/CreateButton.jsx
+++ frontend/my-react-app/src/components/buttons/create/CreateButton.jsx
@@ -1,12 +1,14 @@
-const CreateButton = ({ value, handleListApi}) => {
+const CreateButton = ({ value, handleListApi }) => {
   return (
     <button
-    onClick={handleListApi}
+      onClick={handleListApi}
       type="submit"
-      className="rounded-md border bg-slate-600 px-8 py-1 text-sky-50 hover:bg-slate-500 capitalize"
+      className="rounded-md border bg-slate-600 px-8 py-1 capitalize text-sky-50 hover:bg-slate-500"
     >
       {value}
     </button>
+
+    
   );
 };
 
